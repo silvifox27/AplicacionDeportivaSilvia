ea56943c2ac375c70f39cecc4f19911bf5de355e
silvifox27 <131966685+silvifox27@users.noreply.github.com>
2023-11-07 13:34:19 +0100

    Silvia cronometro

--- app/src/main/java/com/example/aplicaciondeportiva/CronometroMain.java
+++ app/src/main/java/com/example/aplicaciondeportiva/CronometroMain.java
@@ -1,14 +1,20 @@
 package com.example.aplicaciondeportiva;
 
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CronometroMain extends AppCompatActivity {
 
+    ProgressBar barraProgreso;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        
+
+
     }
 }
